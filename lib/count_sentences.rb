baf5d77420f976b3f68b7d66f07cce6cbc04bf01
Jerry Fitzner <jfitzner1@gmail.com>
2022-09-05 12:38:30 -0400

    commit tree c4268dd37194dcc8404c83c9a549c4513e3a8860

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = 0
+    punc = [".", "!", "?"]
+    self.chars.each.with_index do |char, index|
+      # puts char, index
+      if punc.include?(char) && char != self[index+1]
+        sentences += 1
+      end
+    end
+    sentences
   end
+    # self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
 end
